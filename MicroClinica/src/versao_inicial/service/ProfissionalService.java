1a540d40bf67078c632681e6e92e671470a459cf
Erick Borges dos Santos <erickborges04@hotmail.com>
2025-09-04 17:28:41 -0300

    Iniciando service

---
+++ MicroClinica/src/versao_inicial/service/ProfissionalService.java
@@ -0,0 +1,15 @@
+package versao_inicial.service;
+
+import java.util.List;
+import versao_inicial.model.Profissional;
+
+public class ProfissionalService {
+    private List<Profissional> profissionaisDB;
+    private int contadorRequisicao;
+
+    public ProfissionalService(){}
+
+    /*public boolean verificarDisponibilidade() {
+        //add logica
+    }*/
+}
